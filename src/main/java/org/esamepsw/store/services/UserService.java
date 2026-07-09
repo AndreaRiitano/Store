0d8e0423251ca69c05adbdb31c6618060c516b06
e <rtnndr04r01i872g@studenti.unical.it>
2026-07-09 20:56:07 +0200

    user service

---
+++ src/main/java/org/esamepsw/store/services/UserService.java
@@ -0,0 +1,43 @@
+package org.esamepsw.store.services;
+
+import org.esamepsw.store.utilities.exceptions.userexceptions.UserAlreadyExistsException;
+import org.esamepsw.store.entities.User;
+import org.esamepsw.store.repositories.UserRepository;
+import org.esamepsw.store.utilities.exceptions.userexceptions.UserAlreadyExistsException;
+import org.esamepsw.store.utilities.exceptions.userexceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = false)
+    public User addUser(User user) throws UserAlreadyExistsException {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UserAlreadyExistsException();
+        }
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserById(Long id) throws UserNotFoundException {
+        if(userRepository.existsById(id)) {
+            return userRepository.findById(id).get();
+        }else {
+            throw new UserNotFoundException();
+        }
+
+    }
+
+}
